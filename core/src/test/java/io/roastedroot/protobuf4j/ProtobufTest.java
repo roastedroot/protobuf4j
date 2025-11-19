9049d1af465688a848c4c343820176f62374960e
Carles Arnal <carlesarnal92@gmail.com>
2025-11-19 11:35:55 +0100

    Implement well known types support (#18)

--- core/src/test/java/io/roastedroot/protobuf4j/ProtobufTest.java
+++ core/src/test/java/io/roastedroot/protobuf4j/ProtobufTest.java
@@ -449,4 +449,117 @@ public class ProtobufTest {
                 .findFirst()
                 .orElse(null);
     }
+
+    @Test
+    public void shouldHandleProtoWithoutPackageName() throws Exception {
+        // Arrange: Test Apicurio Registry use case - proto without package name
+        FileSystem fs =
+                ZeroFs.newFileSystem(
+                        Configuration.unix().toBuilder().setAttributeViews("unix").build());
+        var workdir = fs.getPath(".");
+        Files.write(workdir.resolve("no_package.proto"), protoContent("no_package.proto"));
+
+        // Act
+        DescriptorProtos.FileDescriptorSet descriptorSet =
+                Protobuf.getDescriptors(workdir, List.of("no_package.proto"));
+
+        // Assert
+        assertNotNull(descriptorSet);
+        assertEquals(1, descriptorSet.getFileCount());
+        assertEquals("no_package.proto", descriptorSet.getFile(0).getName());
+        assertEquals("", descriptorSet.getFile(0).getPackage()); // No package name
+        assertEquals(1, descriptorSet.getFile(0).getMessageTypeCount());
+        assertEquals("SimpleMessage", descriptorSet.getFile(0).getMessageType(0).getName());
+    }
+
+    @Test
+    public void shouldHandleWellKnownTypeImport_Timestamp() throws Exception {
+        // Arrange: Test well-known type import (google.protobuf.Timestamp)
+        FileSystem fs =
+                ZeroFs.newFileSystem(
+                        Configuration.unix().toBuilder().setAttributeViews("unix").build());
+        var workdir = fs.getPath(".");
+        Files.write(workdir.resolve("with_timestamp.proto"), protoContent("with_timestamp.proto"));
+
+        // Act
+        DescriptorProtos.FileDescriptorSet descriptorSet =
+                Protobuf.getDescriptors(workdir, List.of("with_timestamp.proto"));
+
+        // Assert
+        assertNotNull(descriptorSet);
+        // Should include both with_timestamp.proto and google/protobuf/timestamp.proto
+        assertTrue(descriptorSet.getFileCount() >= 1);
+
+        // Find the main proto
+        DescriptorProtos.FileDescriptorProto mainProto =
+                descriptorSet.getFileList().stream()
+                        .filter(fd -> fd.getName().equals("with_timestamp.proto"))
+                        .findFirst()
+                        .orElse(null);
+
+        assertNotNull(mainProto);
+        assertEquals("events", mainProto.getPackage());
+        assertEquals(1, mainProto.getMessageTypeCount());
+        assertEquals("Event", mainProto.getMessageType(0).getName());
+    }
+
+    @Test
+    public void shouldBuildFileDescriptorWithTimestamp() throws Exception {
+        // Arrange: Test buildFileDescriptors with well-known type
+        FileSystem fs =
+                ZeroFs.newFileSystem(
+                        Configuration.unix().toBuilder().setAttributeViews("unix").build());
+        var workdir = fs.getPath(".");
+        Files.write(workdir.resolve("with_timestamp.proto"), protoContent("with_timestamp.proto"));
+
+        // Act
+        List<FileDescriptor> descriptors =
+                Protobuf.buildFileDescriptors(workdir, List.of("with_timestamp.proto"));
+
+        // Assert
+        assertNotNull(descriptors);
+        FileDescriptor mainDescriptor = findDescriptor(descriptors, "with_timestamp.proto");
+        assertNotNull(mainDescriptor);
+        assertEquals("events", mainDescriptor.getPackage());
+        assertEquals(1, mainDescriptor.getMessageTypes().size());
+        assertEquals("Event", mainDescriptor.getMessageTypes().get(0).getName());
+
+        // Verify the Timestamp field
+        assertEquals(
+                2, mainDescriptor.getMessageTypes().get(0).getFields().size());
+    }
+
+    @Test
+    public void shouldHandleMultipleWellKnownTypes() throws Exception {
+        // Arrange: Test multiple well-known type imports
+        FileSystem fs =
+                ZeroFs.newFileSystem(
+                        Configuration.unix().toBuilder().setAttributeViews("unix").build());
+        var workdir = fs.getPath(".");
+        Files.write(
+                workdir.resolve("multiple_wellknown.proto"),
+                protoContent("multiple_wellknown.proto"));
+
+        // Act
+        DescriptorProtos.FileDescriptorSet descriptorSet =
+                Protobuf.getDescriptors(workdir, List.of("multiple_wellknown.proto"));
+
+        // Assert
+        assertNotNull(descriptorSet);
+        assertTrue(descriptorSet.getFileCount() >= 1);
+
+        // Find the main proto
+        DescriptorProtos.FileDescriptorProto mainProto =
+                descriptorSet.getFileList().stream()
+                        .filter(fd -> fd.getName().equals("multiple_wellknown.proto"))
+                        .findFirst()
+                        .orElse(null);
+
+        assertNotNull(mainProto);
+        assertEquals("complex", mainProto.getPackage());
+        assertEquals(1, mainProto.getMessageTypeCount());
+        assertEquals("ComplexMessage", mainProto.getMessageType(0).getName());
+        assertEquals(1, mainProto.getServiceCount());
+        assertEquals("ComplexService", mainProto.getService(0).getName());
+    }
 }
