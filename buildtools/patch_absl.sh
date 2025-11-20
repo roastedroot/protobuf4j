592ae6b5be4368c743793e301997e1353456abfc
Carles Arnal <carlesarnal92@gmail.com>
2025-11-20 14:38:27 +0100

    Better reuse scripts

--- buildtools/patch_absl.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-set -euxo pipefail
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-cd ${SCRIPT_DIR}/protobuf/third_party/abseil-cpp/ && patch -p1 < ${SCRIPT_DIR}/patch-absl.txt
