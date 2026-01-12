7609052655f381feda0a22d3e92ce1264629c6bc
Ash <73482956+ascopes@users.noreply.github.com>
2026-01-12 11:56:55 +0000

    Chore: amend scripts to comply with shellcheck (#40)
    * Add shellcheck to CI workflow to lint build scripts
    
    * Amend scripts to pass shellcheck and best practises
    
    - Use /usr/bin/env bash to use user-installed bash rather than system-installed bash.
      This avoids problems by pulling in an outdated version of bash if the user manages
      it via homebrew (e.g. on MacOS) or other configurations, as it ensures the bash
      that appears first in the $PATH is the one that is always used.
    - Fix accidental whitespace splitting of paths in several scripts by enforcing the
      correct required string quoting.
    - Replace uses of wget with curl to use a single mechanism for fetching URL contents
      across the build scripts.
    - Use --fail with curl to ensure the script aborts if an error occurs fetching a URL.
    - Remove unneeded use of 'cat' where redirections suffice.
    - Remove unneeded use of 'sed' where regex will suffice.
    - Detect builds for unsupported architectures and fail-fast rather than falling back to
      an x86-64 build.
    - Disable variable expansion on heredoc to avoid risk of accidental interpolation in
      the future.
    
    * Simplify logic for version matching
    
    * Use --progress=plain for docker so the full output is preserved when in a terminal

--- buildtools-common/get_grpcjava.sh
+++ buildtools-common/get_grpcjava.sh
@@ -1,9 +1,12 @@
-#! /bin/bash
+#!/usr/bin/env bash
 set -euxo pipefail
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-mkdir -p ${SCRIPT_DIR}/grpcjava
-curl -L https://github.com/grpc/grpc-java/archive/refs/tags/$(cat ${SCRIPT_DIR}/grpcjava-version.txt | awk '{$1=$1};1').tar.gz | tar -xz --strip-components 1 -C ${SCRIPT_DIR}/grpcjava
+mkdir -p "${SCRIPT_DIR}/grpcjava"
 
-cp -R ${SCRIPT_DIR}/grpcjava/compiler/src/java_plugin/cpp ${SCRIPT_DIR}/protobuf/src/grpcjava
+GRPC_VERSION=$(awk '{$1=$1};1' < "${SCRIPT_DIR}/grpcjava-version.txt")
+curl --fail -L "https://github.com/grpc/grpc-java/archive/refs/tags/${GRPC_VERSION}.tar.gz" \
+    | tar -xz --strip-components 1 -C "${SCRIPT_DIR}/grpcjava"
+
+cp -R "${SCRIPT_DIR}/grpcjava/compiler/src/java_plugin/cpp" "${SCRIPT_DIR}/protobuf/src/grpcjava"
--- buildtools-common/get_protobuf.sh
+++ buildtools-common/get_protobuf.sh
@@ -1,13 +1,13 @@
-#! /bin/bash
+#!/usr/bin/env bash
 set -euxo pipefail
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 # Remove existing protobuf directory if it exists
-rm -rf ${SCRIPT_DIR}/protobuf
+rm -rf "${SCRIPT_DIR}/protobuf"
 
 # Clone the protobuf repository with submodules to get abseil-cpp
-PROTOBUF_VERSION=$(cat ${SCRIPT_DIR}/protobuf-version.txt | awk '{$1=$1};1')
-git clone --depth 1 --branch ${PROTOBUF_VERSION} https://github.com/protocolbuffers/protobuf.git ${SCRIPT_DIR}/protobuf
-cd ${SCRIPT_DIR}/protobuf
+PROTOBUF_VERSION=$(awk '{$1=$1};1' < "${SCRIPT_DIR}/protobuf-version.txt")
+git clone --depth 1 --branch "${PROTOBUF_VERSION}" https://github.com/protocolbuffers/protobuf.git "${SCRIPT_DIR}/protobuf"
+cd "${SCRIPT_DIR}/protobuf"
 git submodule update --init --recursive
