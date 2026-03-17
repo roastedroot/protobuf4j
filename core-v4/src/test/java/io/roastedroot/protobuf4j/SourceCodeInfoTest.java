1e7969fac69e3b2ae0f3d6704303a1ce5bec39ee
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2026-03-17 09:02:32 +0000

    Enable comments in generated code (#61)
    * enable comments in generated code
    
    * spotless

---
+++ core-v4/src/test/java/io/roastedroot/protobuf4j/SourceCodeInfoTest.java
@@ -0,0 +1,14 @@
+package io.roastedroot.protobuf4j;
+
+import io.roastedroot.protobuf4j.test.AbstractSourceCodeInfoTest;
+import io.roastedroot.protobuf4j.test.ProtobufTestAdapter;
+import io.roastedroot.protobuf4j.v4.V4ProtobufTestAdapter;
+import java.nio.file.Path;
+
+public class SourceCodeInfoTest extends AbstractSourceCodeInfoTest {
+
+    @Override
+    protected ProtobufTestAdapter createAdapter(Path workdir) {
+        return new V4ProtobufTestAdapter(workdir);
+    }
+}
